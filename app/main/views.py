3c9ffed3091013e2121d232f1f1cd8d54c8003a9
NatashaSenah <tashsenah@gmail.com>
2018-09-10 15:22:00 +0300

    add the modified app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,14 @@
 from flask_login import login_required
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Reviews, User
+@main.route('/pitch/review/new/<int:id>', methods = ['GET','POST'])
 @login_required
 def new_review(id):
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
