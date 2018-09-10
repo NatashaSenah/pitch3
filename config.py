c4f761797ed466ad2de6816f50ff38e7512caa3c
NatashaSenah <tashsenah@gmail.com>
2018-09-10 14:33:03 +0300

    add the modified app/models

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://student:learn@localhost/pitch3'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://student:learn@localhost/pitch3'
 
 
 class ProdConfig(Config):
