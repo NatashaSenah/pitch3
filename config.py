5bfe4307b66ad5c007fd4f64ce4425e6fb844b42
NatashaSenah <tashsenah@gmail.com>
2018-09-10 15:42:41 +0300

    add the configurations app/__init__.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://student:learn@localhost/pitch3'
-
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     pass
