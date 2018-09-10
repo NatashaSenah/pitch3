e40c29ecdd22c170f48d756d1d06e6c2a66253e3
NatashaSenah <tashsenah@gmail.com>
2018-09-10 15:40:46 +0300

    add the modified app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,20 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
      from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     app = Flask(__name__)
-
+    # configure UploadSet
+    configure_uploads(app,photos)
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
